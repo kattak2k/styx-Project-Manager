404f4ff777656a5c7a8a7a49af20fa18eb4c41ad
Kiran K <kkiran.ror@gmail.com>
2016-10-04 04:39:05 +0000

    Created User-Task association, updated controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  # if a user is deleted, then the tasks related must also be deleted, i.e. the reason for dependent: :destroy
+  has_many :tasks, dependent: :destroy  
 end
